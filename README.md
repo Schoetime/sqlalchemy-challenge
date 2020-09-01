275611bca983930a8b1321a3eba4ce9231cb995d
Jeremy Schoenberg <jeremy.schoenberg@gmail.com>
2020-08-31 23:22:18 -0700

    adding app.py with analysis and updated read.me

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 In this homework assignment, I completed some data analysis of temperature data in Hawaii over the past year. I graphed rainfall in inches over that time period as well as the most common temperatures at one of the weather stations.
 
+I then attempted to recreate that analysis in a flask app. I started by copying the structural code from one of the student activities and changed the connection to the correct file. After that was all set up, I attempted to return JSON lists of each query. I was unable to complete the start date and start/end date range queries but will attempt to finish them when I have more time.
--- hawaiiapp.py
+++ hawaiiapp.py
@@ -48,19 +48,48 @@ def welcome():
 def precipitation():
     session = Session(engine)
 
-    ##Analysis goes here
+    #Convert the query results to a dictionary using date as the key and prcp as the value
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    
+    session.close()
+
+    #create the dictionary
+    all_prcp = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        all_prcp.append(prcp_dict)
+
+    return jsonify(all_prcp)
 
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
 
-    ##Analysis goes here
+    ##Return List of Stations in JSON
+    results = session.query(Station.name, Station.station).group_by(Station.station).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_results = list(np.ravel(results))
+
+    return jsonify(all_results)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     session = Session(engine)
 
-    ##Analysis goes here
+    ##return the list of temperature obeservations of the most active station for the last year
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23', Measurement.station == 'USC00519281').order_by(Measurement.date).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_results = list(np.ravel(results))
+
+    return jsonify(all_results)
 
 @app.route("/api/v1.0/<start>")
 def start():
